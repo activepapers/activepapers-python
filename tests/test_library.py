7df45048bcadf79b56b7db3b4c3ee0456e5bf0a0
Konrad Hinsen <konrad.hinsen@fastmail.net>
2014-07-15 17:32:42 +0200

    Python 3 compatibility fix

--- tests/test_library.py
+++ tests/test_library.py
@@ -5,6 +5,7 @@ import tempdir
 
 from activepapers.storage import ActivePaper
 from activepapers import library
+from activepapers.utility import ascii
 
 def test_figshare_download():
     with tempdir.TempDir() as t:
@@ -12,7 +13,7 @@ def test_figshare_download():
         local_name = library.find_in_library("doi:10.6084/m9.figshare.692144")
         assert local_name == os.path.join(t, "10.6084/m9.figshare.692144.ap")
         paper = ActivePaper(local_name)
-        assert paper.code_group['python-packages/immutable/__init__'].attrs['ACTIVE_PAPER_DATATYPE'] == 'module'
+        assert ascii(paper.code_group['python-packages/immutable/__init__'].attrs['ACTIVE_PAPER_DATATYPE']) == 'module'
         paper.close()
 
 def test_zenodo_download():
@@ -21,5 +22,5 @@ def test_zenodo_download():
         local_name = library.find_in_library("doi:10.5281/zenodo.7648")
         assert local_name == os.path.join(t, "10.5281/zenodo.7648.ap")
         paper = ActivePaper(local_name)
-        assert paper.code_group['python-packages/mosaic/__init__'].attrs['ACTIVE_PAPER_DATATYPE'] == 'module'
+        assert ascii(paper.code_group['python-packages/mosaic/__init__'].attrs['ACTIVE_PAPER_DATATYPE']) == 'module'
         paper.close()
